e9c2a9dac73c83ece8774aa0245a8050ab54bd3f
SkylerTyler1337 <tylernichols409@gmail.com>
2014-11-11 11:36:57 -0600

    Create SkylerPGM.java

---
+++ src/me/skylertyler/skylerpgm/SkylerPGM.java
@@ -0,0 +1,16 @@
+package me.skylertyler.skylerpgm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkylerPGM extends JavaPlugin{
+
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
