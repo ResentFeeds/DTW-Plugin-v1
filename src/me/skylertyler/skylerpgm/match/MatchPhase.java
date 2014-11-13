416234b596253768e7925b258d09425c4ecec913
SkylerTyler1337 <tylernichols409@gmail.com>
2014-11-12 22:40:34 -0600

    added Match MatchPhase files

---
+++ src/me/skylertyler/skylerpgm/match/MatchPhase.java
@@ -0,0 +1,57 @@
+import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
+
+public enum MatchPhase {
+
+	Cycling(ChatColor.RED, "Cycling"), Playing(ChatColor.YELLOW, "Playing"), Restarting(
+			ChatColor.GOLD, "Restarting"), pregame(ChatColor.GREEN, "PreGame");
+
+	private ChatColor color;
+	private String text;
+	private DyeColor Dcolor;
+
+
+    MatchPhase(ChatColor color, String text) {
+		this.color = color;
+		this.text = text;
+	}
+
+	private MatchPhase(DyeColor Dcolor) {
+		this.Dcolor = Dcolor;
+	}
+
+	public ChatColor getColor() {
+		return this.color;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	@Override
+	public String toString() {
+		return getColor() + getText();
+	}
+
+	public DyeColor getDyeColorFromMatchPhase() {
+		switch (this) {
+		case Cycling:
+			return DyeColor.RED;
+		case Playing:
+			return DyeColor.YELLOW;
+		case Restarting:
+			return DyeColor.ORANGE;
+		case pregame:
+			return DyeColor.GREEN;
+		default:
+			return DyeColor.WHITE;
+
+		}
+	}
+
+
+
+	public DyeColor getDyeColor() {
+		return this.Dcolor;
+	}
+}
