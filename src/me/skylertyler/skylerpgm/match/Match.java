a477afa86523e1e213df5cee557541973f8c3995
SkylerTyler1337 <tylernichols409@gmail.com>
2014-11-12 22:39:34 -0600

    Create Match.java

---
+++ src/me/skylertyler/skylerpgm/match/Match.java
@@ -0,0 +1,25 @@
+package me.skylertyler.skylerpgm.match;
+
+public class Match {
+
+	private int matchid;
+	private MatchPhase phase;
+
+	public Match(int match) {
+		this.matchid = match;
+		this.phase = MatchPhase.pregame;
+	}
+
+	public int getMatchID() {
+		return this.matchid;
+	}
+
+	public MatchPhase getMatchPhase() {
+		return this.phase;
+	}
+
+	public void setMatchPhase(MatchPhase phase) {
+		this.phase = phase;
+	}
+
+}
