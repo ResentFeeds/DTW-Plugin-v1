26f86cd9d2b201455cb254e6793ab23292ce6933
SkylerTyler1337 <tylernichols409@gmail.com>
2014-11-12 22:42:01 -0600

    Create Log.java

---
+++ src/me/skylertyler/skylerpgm/utils/Log.java
@@ -0,0 +1,25 @@
+package me.skylertyler.skylerpgm.utils;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Log {
+
+	private static Logger logger = Logger.getLogger("Minecraft");
+
+	public static void infoMSG(String message) {
+		logger.log(Level.INFO, message);
+	}
+
+	public static void warningMSG(String message) {
+		logger.log(Level.WARNING, message);
+	}
+
+	public static void severeMSG(String message) {
+		logger.log(Level.SEVERE, message);
+	}
+
+	public static Logger getLogger() {
+		return logger;
+	}
+}
